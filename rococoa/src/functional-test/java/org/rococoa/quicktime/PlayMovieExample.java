297c81d80f8bba76478408cdbc9cda160e0a2047
dmcg <dmcg@e1dc94fc-aea3-11de-8db9-a33e7b47b29e>
2008-02-25 12:25:45 +0000

    Example tarting

--- rococoa/src/functional-test/java/org/rococoa/quicktime/PlayMovieExample.java
+++ rococoa/src/functional-test/java/org/rococoa/quicktime/PlayMovieExample.java
@@ -1,15 +1,11 @@
 package org.rococoa.quicktime;
 
-import java.awt.event.*;
 import java.io.File;
-import java.util.concurrent.CyclicBarrier;
 
 import javax.swing.JFrame;
 
-import org.rococoa.*;
+public class PlayMovieExample {
 
-public class MovieComponentTest extends RococoaTestCase {
-    
     static final File FILE = new File("testdata/DrWho.mov");
 
     static {
@@ -17,40 +13,22 @@ public class MovieComponentTest extends RococoaTestCase {
         @SuppressWarnings("unused")
         QTKit instance = QTKit.instance;
     }
-    
-    public void testShow() throws Exception {
-        assertTrue(FILE.exists());
 
+	public static void main(String[] args) {
         QTMovieView movieView = QTMovieView.CLASS.create();
         movieView.setControllerVisible(true);
         movieView.setPreservesAspectRatio(true);
 
         MovieComponent component = new MovieComponent(movieView);
         JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(component);
 
         QTMovie movie = QTMovie.movieWithFile_error(FILE.getPath(), null);
         movieView.setMovie(movie);                
-        movie.gotoEnd();
-        showAndWaitUntilClosed(frame);
-    }
-
-    private void showAndWaitUntilClosed(JFrame frame)
-            throws Exception {
+        movie.gotoBeginning();
         frame.pack();
         frame.setVisible(true);
-        
-        final CyclicBarrier done = new CyclicBarrier(2); 
-        frame.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-            	try {
-					done.await();
-				} catch (Exception x) {
-					throw new RuntimeException(x);
-				}
-            }});
-        
-        done.await();
     }
 
 }
