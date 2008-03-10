086feed5fe23da8dcdf81600c26b8c68860eecee
dmcg <dmcg@e1dc94fc-aea3-11de-8db9-a33e7b47b29e>
2008-03-10 08:45:36 +0000

    Fix 'Timezone issue in CFStringTest' https://rococoa.dev.java.net/issues/show_bug.cgi?id=4 Use string rather than date as target.

--- trunk/rococoa/src/test/java/org/rococoa/CFStringTest.java
+++ trunk/rococoa/src/test/java/org/rococoa/CFStringTest.java
@@ -24,10 +24,10 @@ package org.rococoa;
 public class CFStringTest extends NSTestCase {
     
     public void testCFStringImpersonatesNSString() throws Exception {
-        CFString s = new CFString("2001-03-24 10:45:32 +0000");        
-        ID clas = Foundation.nsClass("NSDate");
-        ID instance = (ID) Foundation.sendReturnsID(clas, "dateWithString:", s);
-        assertEquals("2001-03-24 10:45:32 +0000",
+        CFString s = new CFString("Hello World");        
+        ID clas = Foundation.nsClass("NSString");
+        ID instance = (ID) Foundation.sendReturnsID(clas, "stringWithString:", s);
+        assertEquals("Hello World",
                 Foundation.toString(Foundation.sendReturnsID(instance, "description")));
     }
     
