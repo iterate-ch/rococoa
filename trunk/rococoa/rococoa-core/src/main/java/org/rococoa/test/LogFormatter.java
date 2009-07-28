bfcb1132cec6a520bc6309ca0643fad34f3db56d
dmcg <dmcg@e1dc94fc-aea3-11de-8db9-a33e7b47b29e>
2009-07-28 18:48:28 +0000

    Logging rationalisation

--- trunk/rococoa/rococoa-core/src/main/java/org/rococoa/test/LogFormatter.java
+++ trunk/rococoa/rococoa-core/src/main/java/org/rococoa/test/LogFormatter.java
@@ -21,8 +21,6 @@ package org.rococoa.test;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.text.MessageFormat;
-import java.util.Date;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
@@ -33,11 +31,6 @@ import java.util.logging.LogRecord;
 public class LogFormatter extends Formatter
 {
 
-    Date dat = new Date();
-    private final static String format = "{0,time} ";
-    private MessageFormat formatter;
-
-    private Object args[] = new Object[1];
 
     // Line separator string. This is the value of the line.separator
     // property at the moment that the SimpleFormatter was created.
@@ -55,16 +48,7 @@ public class LogFormatter extends Formatter
     public synchronized String format(LogRecord record)
     {
         StringBuffer sb = new StringBuffer();
-        // Minimize memory allocations here.
-        dat.setTime(record.getMillis());
-        args[0] = dat;
-        StringBuffer text = new StringBuffer();
-        if (formatter == null)
-        {
-            formatter = new MessageFormat(format);
-        }
-        formatter.format(args, text, null);
-        sb.append(text);
+        sb.append(shortName(Thread.currentThread())).append('\t');
 
         try {
             sb.append(Class.forName(record.getSourceClassName()).getSimpleName());
@@ -98,6 +82,11 @@ public class LogFormatter extends Formatter
             }
         }
         return sb.toString();
+    }
+
+    private String shortName(Thread thread) {
+	String name = thread.getName();
+	return name.length() <= 7 ? name : name.substring(0, 7);
     }
 
     protected void appendExtras(LogRecord record, StringBuffer sb) {
