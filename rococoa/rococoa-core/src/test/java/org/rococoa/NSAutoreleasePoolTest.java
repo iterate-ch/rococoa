386aa8e94db9308a4cce851a441806a3f931864c
dmcg <dmcg@e1dc94fc-aea3-11de-8db9-a33e7b47b29e>
2009-07-28 11:24:09 +0000

    More characterisation of NSAutoreleasePool

--- rococoa/rococoa-core/src/test/java/org/rococoa/NSAutoreleasePoolTest.java
+++ rococoa/rococoa-core/src/test/java/org/rococoa/NSAutoreleasePoolTest.java
@@ -2,6 +2,7 @@ package org.rococoa;
 
 import java.lang.ref.WeakReference;
 
+import org.junit.After;
 import org.junit.Test;
 import org.rococoa.cocoa.foundation.NSAutoreleasePool;
 import org.rococoa.test.RococoaTestCase;
@@ -9,24 +10,56 @@ import org.rococoa.test.RococoaTestCase;
 
 public class NSAutoreleasePoolTest {
 
-    @Test public void drainDoesntLeadToCrashInFinalize() {
-	NSAutoreleasePool pool = NSAutoreleasePool.new_();
-	RococoaTestCase.assertRetainCount(1, pool);
+    private NSAutoreleasePool pool;
+    private final ID idString;
+    
+    public NSAutoreleasePoolTest() {
+        idString = Foundation.cfString("test");
+        pool = NSAutoreleasePool.new_();
+	Foundation.cfRetain(idString); // keep from being dealloc'd
+        Foundation.sendReturnsID(idString, "autorelease");
+        RococoaTestCase.assertRetainCount(2, idString); 
+    }
+    
+    @After public void tearDown() {
+	Foundation.cfRelease(idString);
+    }
+
+    @Test public void drainReleasesContents() {	
 	pool.drain();
+	RococoaTestCase.assertRetainCount(1, idString);
+    }
+
+    @Test public void releaseReleasesContents() {	
 	pool.release();
+	RococoaTestCase.assertRetainCount(1, idString);
+    }
+    
+    @Test public void cfReleaseReleasesContents() {	
 	Foundation.cfRelease(pool.id());
-	RococoaTestCase.assertRetainCount(1, pool);
-	
-        // wait until object has been GC'd
+	RococoaTestCase.assertRetainCount(1, idString);
+    }
+
+    @Test public void finalizeDoesntRelease() {	
         WeakReference<Object> reference = new WeakReference<Object>(pool);
         pool = null;
         while (reference.get() != null) {
             RococoaTestCase.gc();
         }
-        
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 10; i++) {
             RococoaTestCase.gc();
+            RococoaTestCase.assertRetainCount(2, idString);
         }
+    }
+    
+    @Test public void nothingAffectsReferenceCount() {
+        RococoaTestCase.assertRetainCount(1, pool);
+        pool.drain();
+        RococoaTestCase.assertRetainCount(1, pool);
+        pool.release();
+        RococoaTestCase.assertRetainCount(1, pool);
+        Foundation.cfRelease(pool.id());
+        RococoaTestCase.assertRetainCount(1, pool);
     }
     
 }
