3aa18f8851c1eb84aff9dd364f67381c247a7fb4
dkocher <dkocher@e1dc94fc-aea3-11de-8db9-a33e7b47b29e>
2009-09-07 21:15:09 +0000

    Check log level.

--- rococoa/rococoa-core/src/main/java/org/rococoa/Foundation.java
+++ rococoa/rococoa-core/src/main/java/org/rococoa/Foundation.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2007, 2008 Duncan McGregor
- * 
+ *
  * This file is part of Rococoa, a library to allow Java to talk to Cocoa.
- * 
+ *
  * Rococoa is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Rococoa is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with Rococoa.  If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
 package org.rococoa;
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
@@ -40,40 +40,40 @@ import com.sun.jna.Native;
 
 /**
  * The core of Rococoa - statics to handle selectors and messaging at a function call level.
- * 
+ *
  * Marshalling of Java types to C types is handled by JNA. Marshalling of Java
  * type to Objective-C types is handled by RococoaTypeMapper.
- * 
+ *
  * Not to be confused with the Mac Foundation or Core Foundation frameworks, most
- * users shouldn't need to access this class directly. 
- * 
+ * users shouldn't need to access this class directly.
+ *
  * @author duncan
  */
 @SuppressWarnings("nls")
 public abstract class Foundation {
-    
+
     private static Logger logging = LoggerFactory.getLogger("org.rococoa.foundation");
-   
+
     private static final FoundationLibrary foundationLibrary;
     private static final MsgSendLibrary messageSendLibrary;
     private static final RococoaLibrary rococoaLibrary;
-    
+
     private static final Map<String, Selector> selectorCache = new HashMap<String, Selector>();
-    
+
     static {
         logging.trace("Initializing Foundation");
-        
+
         // Set JNA to convert java.lang.String to char* using UTF-8, and match that with
         // the way we tell CF to interpret our char*
         // May be removed if we use toStringViaUTF16
         System.setProperty("jna.encoding", "UTF8");
-        
+
         Map<String, Object> messageSendLibraryOptions = new HashMap<String, Object>(1);
         messageSendLibraryOptions.put(Library.OPTION_INVOCATION_MAPPER, new MsgSendInvocationMapper());
         messageSendLibrary = (MsgSendLibrary) Native.loadLibrary("Foundation", MsgSendLibrary.class, messageSendLibraryOptions);
-                
+
         foundationLibrary = (FoundationLibrary) Native.loadLibrary("Foundation", FoundationLibrary.class);
-        rococoaLibrary = (RococoaLibrary) Native.loadLibrary("rococoa", RococoaLibrary.class);        
+        rococoaLibrary = (RococoaLibrary) Native.loadLibrary("rococoa", RococoaLibrary.class);
         logging.trace("exit initializing Foundation");
     }
 
@@ -84,11 +84,11 @@ public abstract class Foundation {
         } finally {
             cfRelease(formatAsCFString);
         }
-    }    
-    
+    }
+
     /**
      * Return a CFString as an ID, toll-free bridged to NSString.
-     * 
+     *
      * Note that the returned string must be freed with {@link #cfRelease(ID)}.
      */
     public static ID cfString(String s) {
@@ -103,27 +103,31 @@ public abstract class Foundation {
             throw new RococoaException(x);
         }
     }
-    
+
     /**
      * Retain the NSObject with id
      */
     public static ID cfRetain(ID id) {
-        logging.trace("calling cfRetain({})", id);
-        return foundationLibrary.CFRetain(id);        
+        if (logging.isTraceEnabled()) {
+            logging.trace("calling cfRetain({})", id);
+        }
+        return foundationLibrary.CFRetain(id);
     }
-    
+
     /**
      * Release the NSObject with id
      */
     public static void cfRelease(ID id) {
-        logging.trace("calling cfRelease({})", id);
+        if (logging.isTraceEnabled()) {
+            logging.trace("calling cfRelease({})", id);
+        }
         foundationLibrary.CFRelease(id);
     }
 
     public static int cfGetRetainCount(ID cfTypeRef) {
         return foundationLibrary.CFGetRetainCount(cfTypeRef);
     }
-    
+
     public static String toString(ID cfString) {
         return toStringViaUTF8(cfString);
     }
@@ -133,37 +137,39 @@ public abstract class Foundation {
         try {
             int lengthInChars = foundationLibrary.CFStringGetLength(cfString);
             int potentialLengthInBytes = 3 * lengthInChars + 1; // UTF16 fully escaped 16 bit chars, plus nul
-            
+
             byte[] buffer = new byte[potentialLengthInBytes];
             byte ok = foundationLibrary.CFStringGetCString(cfString, buffer, buffer.length, StringEncoding.kCFStringEncodingUTF16LE.value);
             if (ok == 0)
                 throw new RococoaException("Could not convert string");
-            return new String(buffer, "UTF-16LE").substring(0, lengthInChars); 
+            return new String(buffer, "UTF-16LE").substring(0, lengthInChars);
         } catch (UnsupportedEncodingException e) {
             throw new RococoaException(e);
         }
     }
-    
+
     static String toStringViaUTF8(ID cfString) {
         int lengthInChars = foundationLibrary.CFStringGetLength(cfString);
         int potentialLengthInBytes = 3 * lengthInChars + 1; // UTF8 fully escaped 16 bit chars, plus nul
-        
+
         byte[] buffer = new byte[potentialLengthInBytes];
         byte ok = foundationLibrary.CFStringGetCString(cfString, buffer, buffer.length, StringEncoding.kCFStringEncodingUTF8.value);
         if (ok == 0)
             throw new RococoaException("Could not convert string");
         return Native.toString(buffer);
     }
-    
+
     /**
      * Get the ID of the NSClass with className
      */
     public static ID getClass(String className) {
-        logging.trace("calling objc_getClass({})", className);
+        if (logging.isTraceEnabled()) {
+            logging.trace("calling objc_getClass({})", className);
+        }
         return foundationLibrary.objc_getClass(className);
     }
-        
-    public static Selector selector(String selectorName) { 
+
+    public static Selector selector(String selectorName) {
         Selector cached = selectorCache.get(selectorName);
         if (cached != null)
             return cached;
@@ -174,7 +180,7 @@ public abstract class Foundation {
 
     /**
      * Send message with selectorName to receiver, passing args, expecting returnType.
-     * 
+     *
      * Note that you are responsible for memory management if returnType is ID.
      */
     public static <T> T send(ID receiver, String selectorName, Class<T> returnType, Object... args) {
@@ -183,20 +189,20 @@ public abstract class Foundation {
 
     /**
      * Send message with selector to receiver, passing args, expecting returnType.
-     * 
+     *
      * Note that you are responsible for memory management if returnType is ID.
      */
     @SuppressWarnings("unchecked")
     public static <T> T send(ID receiver, Selector selector, Class<T> returnType, Object... args) {
         if (logging.isTraceEnabled())
-            logging.trace("sending ({}) {}.{}({})", 
+            logging.trace("sending ({}) {}.{}({})",
                     new Object[] {returnType.getSimpleName(), receiver, selector.getName(), new VarArgsUnpacker(args)});
-        return (T) messageSendLibrary.syntheticSendMessage(returnType, receiver, selector, args);        
+        return (T) messageSendLibrary.syntheticSendMessage(returnType, receiver, selector, args);
     }
-        
+
     /**
      * Convenience as this happens a lot in tests.
-     * 
+     *
      * Note that you are responsible for memory management for the returned ID
      */
     public static ID sendReturnsID(ID receiver, String selectorName, Object... args) {
@@ -213,14 +219,14 @@ public abstract class Foundation {
     public static boolean isMainThread() {
         return MainThreadUtils.isMainThread();
     }
-    
+
     /**
      * Return the result of calling callable on the main Cococoa thread.
      */
     public static <T> T callOnMainThread(final Callable<T> callable) {
         return MainThreadUtils.callOnMainThread(rococoaLibrary, callable);
     }
-    
+
     /**
      * Run runnable on the main Cococoa thread, waiting for completion.
      */
@@ -237,12 +243,12 @@ public abstract class Foundation {
     /**
      * Create an Objective-C object which delegates to callbacks when methods
      * are invoked on it.
-     * 
+     *
      * Object is created with alloc, so is owned by the caller.
      */
     public static ID newOCProxy(OCInvocationCallbacks callbacks) {
         return rococoaLibrary.proxyForJavaObject(
-                callbacks.selectorInvokedCallback, 
+                callbacks.selectorInvokedCallback,
                 callbacks.methodSignatureCallback);
     }
 
@@ -255,11 +261,11 @@ public abstract class Foundation {
         // message. You are responsible for relinquishing ownership of objects
         // you own using release or autorelease. Any other time you receive an
         // object, you must not release it.
-	
+
 	// Note that this does not appear to be an infallible rule - see
 	// https://rococoa.dev.java.net/servlets/ReadMsg?list=dev&msgNo=71
-        return selectorName.startsWith("alloc") || 
-        	selectorName.startsWith("new") || 
+        return selectorName.startsWith("alloc") ||
+        	selectorName.startsWith("new") ||
         	selectorName.toLowerCase().contains("copy");
     }
 
