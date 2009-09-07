7a5dcfca4482d914898be938177d407c3ba84af1
dkocher <dkocher@e1dc94fc-aea3-11de-8db9-a33e7b47b29e>
2009-09-07 20:39:55 +0000

    Check logging level. Check for null values returned from invocation callback implementation.

--- rococoa/rococoa-core/src/main/java/org/rococoa/internal/OCInvocationCallbacks.java
+++ rococoa/rococoa-core/src/main/java/org/rococoa/internal/OCInvocationCallbacks.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2007, 2008 Duncan McGregor
- * 
+ *
  * This file is part of Rococoa, a library to allow Java to talk to Cocoa.
- * 
+ *
  * Rococoa is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Rococoa is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with Rococoa.  If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
 package org.rococoa.internal;
 
 import java.lang.reflect.InvocationTargetException;
@@ -35,63 +35,71 @@ import com.sun.jna.Memory;
 /**
  * Holds the callbacks called when a method is invoked on an Objective-C proxy
  * for a Java object.
- * 
+ *
  * When a message is sent to an OC object first it is sent
  * methodSignatureForSelector: Our Obj-C proxy forwards this to
  * methodSignatureCallback; we build a method signature string in Java
  * corresponding to the Java method and return it.
- * 
+ *
  * The object is then sent forwardInvocation: passing an NSInvocation. It
  * forwards this to selectorInvokedCallback, which we use to invoke the method
  * on the Java Object.
- * 
+ *
  * @author duncan
- * 
+ *
  */
 @SuppressWarnings("nls")
 public class OCInvocationCallbacks {
 
     private static Logger logging = LoggerFactory.getLogger("org.rococoa.callback");
-        
+
     private final Object javaObject;
-    
+
     /**
      * Called when method is about to be invoked on OC proxy and needs a method signature as String
-     * 
-     * @see http://www.cocoadev.com/index.pl?NSMethodSignature
+     *
+     * @see "http://www.cocoadev.com/index.pl?NSMethodSignature"
      */
-    public final RococoaLibrary.MethodSignatureCallback methodSignatureCallback = 
+    public final RococoaLibrary.MethodSignatureCallback methodSignatureCallback =
         new RococoaLibrary.MethodSignatureCallback() {
             public String callback(String selectorName) {
-                logging.trace("callback wanting methodSignature for selector {}", selectorName);
+                if(logging.isTraceEnabled()) {
+                    logging.trace("callback wanting methodSignature for selector {}", selectorName);
+                }
                 return methodSignatureForSelector(selectorName);
             }
     };
-    
+
     /**
      * Called when method has been invoked on OC proxy and needs to be forwarded to javaObject
      */
-    public final RococoaLibrary.SelectorInvokedCallback selectorInvokedCallback = 
+    public final RococoaLibrary.SelectorInvokedCallback selectorInvokedCallback =
         new RococoaLibrary.SelectorInvokedCallback() {
             public void callback(String selectorName, ID nsInvocation) {
-                logging.trace("callback invoking {} on {}", selectorName, javaObject);
-                callMethod(javaObject, selectorName, 
+                if(logging.isTraceEnabled()) {
+                    logging.trace("callback invoking {} on {}", selectorName, javaObject);
+                }
+                callMethod(javaObject, selectorName,
                         Rococoa.wrap(nsInvocation, NSInvocation.class));
-            };        
+            }
     };
-    
+
     public OCInvocationCallbacks(Object javaObject) {
         this.javaObject = javaObject;
     }
 
     protected String methodSignatureForSelector(String selectorName) {
         Method method = methodForSelector(selectorName);
-        return method == null ? 
+        return method == null ?
                 null :
                 ocMethodSignatureAsString(method);
     }
 
     protected Method methodForSelector(String selectorName) {
+        if(null == selectorName) {
+            logging.error("methodForSelector called with null selectorName");
+            return null;
+        }
         int parameterCount = countColons(selectorName);
         String methodName = methodNameForSelector(selectorName);
         try {
@@ -108,7 +116,7 @@ public class OCInvocationCallbacks {
     }
 
     /**
-     * @see http://developer.apple.com/documentation/Cocoa/Conceptual/ObjectiveC/Articles/chapter_13_section_9.html
+     * @see "http://developer.apple.com/documentation/Cocoa/Conceptual/ObjectiveC/Articles/chapter_13_section_9.html"
      */
     protected String ocMethodSignatureAsString(Method method) {
         StringBuilder result = new StringBuilder();
@@ -123,7 +131,7 @@ public class OCInvocationCallbacks {
     private void callMethod(Object o, String selectorName, NSInvocation invocation) {
         try {
             Method method = methodForSelector(selectorName);
-            
+
             NSMethodSignature nsMethodSignature = invocation.methodSignature();
             String typeToReturnToObjC = nsMethodSignature.methodReturnType();
 
@@ -131,17 +139,17 @@ public class OCInvocationCallbacks {
                 throw new NoSuchMethodException(String.format(
                         "Number of arguments mismatch for invocation  of selector %s (%s arguments supplied), method %s expects %s",
                         selectorName, nsMethodSignature.numberOfArguments(), method.getName(), method.getParameterTypes().length));
-            
+
             if (typeToReturnToObjC.equals("v") && method.getReturnType() != void.class)
                 throw new NoSuchMethodException(String.format(
                         "Selector %s expects void return, but method %s returns %s",
                         selectorName, method.getName(), method.getReturnType()));
-                
+
             if (method.getReturnType() == void.class && !(typeToReturnToObjC.equals("v")))
                 throw new NoSuchMethodException(String.format(
                         "Method %s returns void, but selector %s expects %s",
                         method.getName(), selectorName, typeToReturnToObjC));
-                
+
             Object[] marshalledArgs = argsForFrom(method, invocation, nsMethodSignature);
             method.setAccessible(true); // needed if implementation is an anonymous subclass of Object
             Object result  = method.invoke(o, marshalledArgs);
@@ -154,10 +162,10 @@ public class OCInvocationCallbacks {
             throw new RococoaException("Exception calling method for selector " + selectorName, e);
         }
     }
-    
+
     private String methodNameForSelector(String selectorName) {
         String candidate =  selectorName.replaceAll(":", "_");
-        return candidate.endsWith("_") ? 
+        return candidate.endsWith("_") ?
                 candidate.substring(0, candidate.length() - 1) :
                 candidate;
     }
@@ -167,9 +175,9 @@ public class OCInvocationCallbacks {
         Object[] result = new Object[parameterTypes.length];
         for (int i = 0; i < result.length; i++) {
             int indexAccountingForSelfAndCmd = 2 + i;
-            result[i] = javaObjectForOCArgument(invocation, 
+            result[i] = javaObjectForOCArgument(invocation,
                     indexAccountingForSelfAndCmd,
-                    nsMethodSignature.getArgumentTypeAtIndex(indexAccountingForSelfAndCmd), 
+                    nsMethodSignature.getArgumentTypeAtIndex(indexAccountingForSelfAndCmd),
                     parameterTypes[i]);
         }
         return result;
@@ -187,7 +195,7 @@ public class OCInvocationCallbacks {
         NSInvocationMapper mapper = NSInvocationMapperLookup.mapperForType(javaParameterType);
         if (mapper == null)
             throw new IllegalStateException(
-                String.format("Don't (yet) know how to marshall argument Objective-C type %s as %s", 
+                String.format("Don't (yet) know how to marshall argument Objective-C type %s as %s",
                         objCArgumentTypeAsString, javaParameterType));
         return mapper.readArgumentFrom(invocation, indexInInvocation, javaParameterType);
     }
@@ -198,12 +206,14 @@ public class OCInvocationCallbacks {
                 throw new IllegalStateException("Java method returned a result, but expected void");// void
             return;
         }
-        
+        if(null == result) {
+            return;
+        }
         Memory buffer = bufferForReturn(typeToReturnToObjC, result);
         if (buffer == null)
             throw new IllegalStateException(
                     String.format("Don't (yet) know how to marshall result %s as Objective-C type %s", result, typeToReturnToObjC));
-            
+
         invocation.setReturnValue(buffer);
     }
 
@@ -220,14 +230,14 @@ public class OCInvocationCallbacks {
         }
         return result;
     }
-    
+
     private String stringForType(Class<?> clas) {
         NSInvocationMapper result = NSInvocationMapperLookup.mapperForType(clas);
         if (result == null)
             throw new RococoaException("Unable to give Objective-C type string for Java type " + clas);
-        return result.typeString(); 
+        return result.typeString();
     }
-    
+
     /*
      * [1] - http://en.wikipedia.org/wiki/Parameter_(computer_science)
      * Although parameters are also commonly referred to as arguments,
