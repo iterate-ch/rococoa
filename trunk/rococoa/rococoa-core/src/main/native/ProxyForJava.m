3f73403f51f69007deefd6cf764eed887e583c42
dkocher <dkocher@e1dc94fc-aea3-11de-8db9-a33e7b47b29e>
2009-09-16 17:32:17 +0000

    Use CFStringGetSystemEncoding() to get char pointer from CFString reference. Otherwise CFStringGetCStringPtr returns NULL if the user has a __CF_USER_TEXT_ENCODING other than kCFStringEncodingMacRoman.

--- trunk/rococoa/rococoa-core/src/main/native/ProxyForJava.m
+++ trunk/rococoa/rococoa-core/src/main/native/ProxyForJava.m
@@ -1,10 +1,31 @@
+/*
+ * Copyright 2007, 2008 Duncan McGregor
+ *
+ * This file is part of Rococoa, a library to allow Java to talk to Cocoa.
+ *
+ * Rococoa is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Rococoa is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Rococoa.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #import "ProxyForJava.h"
-
 
 id proxyForJavaObject(void* methodInvokedCallback, void* methodSignatureCallback) {
 	return [[ProxyForJava alloc] initWithMethodInvokedCallback: methodInvokedCallback methodSignatureCallback: methodSignatureCallback];
 }
+
+@interface ProxyForJava (Private)
++ (char *)cstringPtrForSelector:(CFStringRef) cfstring;
+@end
 
 @implementation ProxyForJava
 
@@ -22,7 +43,7 @@ id proxyForJavaObject(void* methodInvokedCallback, void* methodSignatureCallback
 	SEL selector = [anInvocation selector];
 	NSString* selectorName = NSStringFromSelector(selector);
 	// NSLog(@"forwardInvocation for %@", selectorName);
-	methodInvokedCallback(CFStringGetCStringPtr((CFStringRef) selectorName, 0), anInvocation);
+	methodInvokedCallback([ProxyForJava cstringPtrForSelector:(CFStringRef) selectorName], anInvocation);
 }
 
 - (BOOL)respondsToSelector:(SEL)aSelector {
@@ -34,15 +55,29 @@ id proxyForJavaObject(void* methodInvokedCallback, void* methodSignatureCallback
 - (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector {
 	NSString* selectorName = NSStringFromSelector(aSelector);
 	// NSLog(@"methodSignatureForSelector %@", selectorName);
-	if (aSelector == @selector(hash) || aSelector == @selector(isEqual:))
+	if (aSelector == @selector(hash) || aSelector == @selector(isEqual:)) {
 		return [super methodSignatureForSelector: aSelector];
-	
-	char* methodSignature = methodSignatureCallback(CFStringGetCStringPtr((CFStringRef) selectorName, 0));
-	if (methodSignature == 0) { 
+	}
+	const char* methodSignature = methodSignatureCallback([ProxyForJava cstringPtrForSelector:(CFStringRef) selectorName]);
+	if (NULL == methodSignature) {
+	    // No method with signature of selector not implemented
 		return nil;
 	}
 	NSMethodSignature* result = [NSMethodSignature signatureWithObjCTypes: methodSignature];
 	return result;
+}
+
++ (const char *)cstringPtrForSelector:(CFStringRef) selectorName {
+	static CFStringEncoding encoding;
+	if(0 == encoding) {
+		encoding = CFStringGetSystemEncoding();
+	}
+	const char* selectorNameChar = CFStringGetCStringPtr(selectorName, encoding);
+	if (NULL == selectorNameChar) {
+		NSLog(@"CFStringGetCStringPtr failed for selector %@", selectorName);
+		return NULL;
+	}
+	return selectorNameChar;
 }
 
 @end
