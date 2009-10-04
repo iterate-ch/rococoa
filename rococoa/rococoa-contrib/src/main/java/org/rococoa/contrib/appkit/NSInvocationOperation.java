6d08ccc9f215ed7bb806232260bb621053566875
lordpixel <lordpixel@e1dc94fc-aea3-11de-8db9-a33e7b47b29e>
2009-10-04 00:14:24 +0000

    Add a mapping for NSOperationQueue, including supporting classes NSOperation and NSInvocationOperation. Test cases included.

---
+++ rococoa/rococoa-contrib/src/main/java/org/rococoa/contrib/appkit/NSInvocationOperation.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2009 Duncan McGregor
+ *
+ * This file is part of Rococoa, a library to allow Java to talk to Cocoa.
+ *
+ * Rococoa is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Rococoa is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Rococoa.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.rococoa.contrib.appkit;
+
+import org.rococoa.ID;
+import org.rococoa.NSClass;
+import org.rococoa.Rococoa;
+import org.rococoa.Selector;
+
+/** NSInvocationOperation from Cocoa.
+ *
+ */
+public interface NSInvocationOperation extends NSOperation {
+    public static final _Class CLASS = Rococoa.createClass(NSInvocationOperation.class.getSimpleName(), _Class.class); //$NON-NLS-1$
+    public interface _Class extends NSClass {
+        public NSInvocationOperation alloc();
+    }
+
+    NSInvocationOperation initWithTarget_selector_object(ID target, Selector sel, ID arg);
+
+    ID result();
+}
