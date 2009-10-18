2b01469b75fd9d50213716bd95ecb5537bd38826
lordpixel@mac.com <lordpixel@mac.com@e1dc94fc-aea3-11de-8db9-a33e7b47b29e>
2009-10-18 16:10:02 +0000

    Try to fix the $Id$ tags

--- rococoa/rococoa-contrib/src/main/java/org/rococoa/contrib/dispatch/GCDExecutorService.java
+++ rococoa/rococoa-contrib/src/main/java/org/rococoa/contrib/dispatch/GCDExecutorService.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with Rococoa.  If not, see <http://www.gnu.org/licenses/>.
  *
- * $Id: $
+ * $Id$
  */
 
 package org.rococoa.contrib.dispatch;
