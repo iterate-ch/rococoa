513bf1177c9d1f908b0820fcaec0ba9cec57985f
dkocher@sudo.ch <dkocher@sudo.ch@e1dc94fc-aea3-11de-8db9-a33e7b47b29e>
2009-11-28 19:48:12 +0000

    Update copyright notice [issue 10].

--- trunk/rococoa/rococoa-core/src/main/java/org/rococoa/cocoa/foundation/NSString.java
+++ trunk/rococoa/rococoa-core/src/main/java/org/rococoa/cocoa/foundation/NSString.java
@@ -1,16 +1,13 @@
-/**
- * 
- */
 /*
- * Copyright 2007, 2008 Duncan McGregor
- * 
+ * Copyright 2007, 2008, 2009 Duncan McGregor
+ *
  * This file is part of Rococoa, a library to allow Java to talk to Cocoa.
- * 
+ *
  * Rococoa is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Rococoa is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -19,6 +16,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with Rococoa.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package org.rococoa.cocoa.foundation;
 
 import org.rococoa.Foundation;
