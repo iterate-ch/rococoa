0bf78c6652bcaf0bed17c8d16d7f394648a3fb97
dkocher@sudo.ch <dkocher@sudo.ch@e1dc94fc-aea3-11de-8db9-a33e7b47b29e>
2011-04-04 22:09:00 +0000

    Add testcase for delegate that accepts a NSError** as an argument.

--- rococoa/rococoa-core/src/test/java/org/rococoa/internal/RococoaObjCObjectByReferenceTest.java
+++ rococoa/rococoa-core/src/test/java/org/rococoa/internal/RococoaObjCObjectByReferenceTest.java
@@ -16,12 +16,10 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with Rococoa.  If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
 package org.rococoa.internal;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
+import org.rococoa.ID;
 import org.rococoa.ObjCObject;
 import org.rococoa.ObjCObjectByReference;
 import org.rococoa.Rococoa;
@@ -29,24 +27,53 @@ import org.rococoa.cocoa.foundation.NSAutoreleasePool;
 import org.rococoa.cocoa.foundation.NSNumber;
 import org.rococoa.test.RococoaTestCase;
 
+import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+
+import static org.junit.Assert.assertEquals;
+
 @SuppressWarnings("nls")
 public class RococoaObjCObjectByReferenceTest extends RococoaTestCase {
     private interface TestShunt extends ObjCObject {
         void testNSNumberByReference_with(ObjCObjectByReference reference, int value);
-    };
-    
-    @Test public void test() {
+
+        void testCallbackWithReference(ID delegate);
+    }
+
+    private interface TestShuntDelegate {
+        public void callback(ObjCObjectByReference reference);
+    }
+
+    @Test
+    public void testArgument() {
         NSAutoreleasePool pool = NSAutoreleasePool.new_();
         TestShunt shunt = Rococoa.create("TestShunt", TestShunt.class);
         ObjCObjectByReference reference = new ObjCObjectByReference();
         shunt.testNSNumberByReference_with(reference, 42);
         NSNumber value = reference.getValueAs(NSNumber.class);
         assertEquals(42, value.intValue());
-                
+
         // we better have retained the result by the time it gets back
         assertEquals(3, value.retainCount());
         pool.drain();
         assertEquals(2, value.retainCount());
     }
 
+    @Test
+    public void testDelegate() {
+        NSAutoreleasePool pool = NSAutoreleasePool.new_();
+        TestShunt shunt = Rococoa.create("TestShunt", TestShunt.class);
+        final CountDownLatch count = new CountDownLatch(1);
+        final ObjCObject callback = Rococoa.proxy(new TestShuntDelegate() {
+            public void callback(ObjCObjectByReference reference) {
+                // Success
+                count.countDown();
+            }
+        });
+        final ID delegate = callback.id();
+        shunt.testCallbackWithReference(delegate);
+        assertEquals("Callback to delegate failed", 0, count.getCount());
+        pool.drain();
+    }
 }
