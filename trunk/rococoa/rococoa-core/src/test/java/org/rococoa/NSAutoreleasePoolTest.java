eebb0f6637d027fdf11267037b4d991e69a0d935
dkocher@sudo.ch <dkocher@sudo.ch@e1dc94fc-aea3-11de-8db9-a33e7b47b29e>
2013-10-16 20:55:16 +0000

    Fix secondary thread test making wrong assertion.

--- trunk/rococoa/rococoa-core/src/test/java/org/rococoa/NSAutoreleasePoolTest.java
+++ trunk/rococoa/rococoa-core/src/test/java/org/rococoa/NSAutoreleasePoolTest.java
@@ -25,21 +25,21 @@ import org.rococoa.cocoa.foundation.NSAutoreleasePool;
 import org.rococoa.test.RococoaTestCase;
 
 /**
- * NSAutoreleasePool's behaviour wrt retain counts is interesting. 
- *
- * Mike Swingler - "NSAutoreleasePools are magical, and are not actually 
- * allocated or released objects (though you get a point(er) to an id that does 
+ * NSAutoreleasePool's behaviour wrt retain counts is interesting.
+ * <p/>
+ * Mike Swingler - "NSAutoreleasePools are magical, and are not actually
+ * allocated or released objects (though you get a point(er) to an id that does
  * respond to selectors...but that's no different than an @"some string" constant."
- *
+ * <p/>
  * This test documents the observed behaviour.
  */
 public class NSAutoreleasePoolTest {
 
     private NSAutoreleasePool pool;
-    private final ID idString;
-    
-    
-    @Test public void nothingAffectsReferenceCount() {
+    private ID idString;
+
+    @Test
+    public void nothingAffectsReferenceCount() {
         RococoaTestCase.assertRetainCount(1, pool);
         pool.drain();
         RococoaTestCase.assertRetainCount(1, pool);
@@ -48,39 +48,49 @@ public class NSAutoreleasePoolTest {
         Foundation.cfRelease(pool.id());
         RococoaTestCase.assertRetainCount(1, pool);
     }
-    
+
     public NSAutoreleasePoolTest() {
         idString = Foundation.cfString("test");
         pool = NSAutoreleasePool.new_();
-	Foundation.cfRetain(idString); // keep from being dealloc'd
+        Foundation.cfRetain(idString); // keep from being deallocated
         Foundation.sendReturnsID(idString, "autorelease");
-        RococoaTestCase.assertRetainCount(2, idString); 
-    }
-    
-    @After public void tearDown() {
-	Foundation.cfRelease(idString);
+        RococoaTestCase.assertRetainCount(2, idString);
     }
 
-    @Test public void drainReleasesContents() {	
-	pool.drain();
-	RococoaTestCase.assertRetainCount(1, idString);
+    @After
+    public void tearDown() {
+        Foundation.cfRelease(idString);
     }
 
-    @Test public void releaseReleasesContents() {	
-	pool.release();
-	RococoaTestCase.assertRetainCount(1, idString);
+    @Test
+    public void drainReleasesContents() {
+        pool.drain();
+        RococoaTestCase.assertRetainCount(1, idString);
     }
-    
-    @Test public void cfReleaseReleasesContents() {	
-	Foundation.cfRelease(pool.id());
-	RococoaTestCase.assertRetainCount(1, idString);
+
+    @Test
+    public void releaseReleasesContents() {
+        pool.release();
+        RococoaTestCase.assertRetainCount(1, idString);
     }
-    
-    @Test public void drainOnAnotherThreadDoesntRelease() throws InterruptedException {
+
+    @Test
+    public void cfReleaseReleasesContents() {
+        Foundation.cfRelease(pool.id());
+        RococoaTestCase.assertRetainCount(1, idString);
+    }
+
+    @Test
+    public void drainOnAnotherThreadDoesntRelease() throws InterruptedException {
+        // If you are making Cocoa calls outside of the Application Kit’s main thread—for example
+        // if you create a Foundation-only application or if you detach a thread—you need to create
+        // your own autorelease pool.
         Thread thread = new Thread("test") {
             public void run() {
-                pool.drain();
-            }};
+                NSAutoreleasePool second = NSAutoreleasePool.new_();
+                second.drain();
+            }
+        };
         thread.start();
         thread.join();
         RococoaTestCase.assertRetainCount(2, idString);
